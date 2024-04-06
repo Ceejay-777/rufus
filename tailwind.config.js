396cf61af9037b39d6a1fa3e578cb9adace0d7bc
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-04-06 20:14:26 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/*.jsx",
+    "./src/*.js,",
+    "./src/*.html",
+    "./src/**/*.jsx",
+    "./src/**/*.js",
+    "./src/**/*.html",
+    "./src/**/**/*.html",
+    "./src/**/**/*.jsx",
+    "./src/**/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
