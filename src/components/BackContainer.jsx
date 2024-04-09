a2d18b95f3cdfa85a878044790bcdb1d98ed45b4
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-04-09 22:15:57 +0100

    commit

--- src/components/BackContainer.jsx
+++ src/components/BackContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BackContainer = () => {
   return (
-    <div className='z-50 md:bg-bg3 bg-bg1 fixed w-screen h-screen bg-no-repeat bg-cover'></div>
+    <div className='-z-50 md:bg-bg3 bg-bg1 fixed w-screen h-screen bg-no-repeat bg-cover'></div>
   );
 }
 
